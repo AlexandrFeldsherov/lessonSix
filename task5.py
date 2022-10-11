54a9be39025571d80c6723a8368616d8a173c21e
[AlexandrFeldsherov] <[alexandrfeldsherov@mail.ru]>
2022-10-11 15:42:36 +0300

    v002

--- task5.py
+++ task5.py
@@ -1,1 +1,8 @@
 # 5- Найти произведение пар чисел в списке. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+import math
+lst = [i for i in range (1, 21) ]
+lst_2=lst[::-1]
+lst=list(map(lambda x,y: x*y,lst,lst_2 ))
+print(math.ceil(len(lst)/2))
+lst=lst[:math.ceil(len(lst)/2)]
+print(lst)
